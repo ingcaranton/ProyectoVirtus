4200dbcf33a244191306d8f36f691f13f1ecfa42
vicadi <vicadi7@gmail.com>
2015-12-07 19:21:39 -0500

    inicio de pregunta1

--- public/juego/script-juego.js
+++ public/juego/script-juego.js
@@ -34,9 +34,9 @@ $("#nombreJugador").on("keyup", function(){
     $("#botonJugador").removeAttr('disabled');
   }
 });
-
+var jugador="";
 $("#botonJugador").on("click",function(){
-  var jugador = $("#nombreJugador").val();
+  jugador = $("#nombreJugador").val();
   socket.emit('jugador', jugador);
   $("#jugador").css("display","none");
   $("#cargando").css("display","inherit");
@@ -44,7 +44,32 @@ $("#botonJugador").on("click",function(){
 });
 
 //recibir partida
-socket.on('partida', function(partida){
+socket.on('pregunta1', function(pregunta1,jugador1,jugador2){
   $("#cargando").css("display","none");
   $("#partida").css("display","inherit");
+});
+
+socket.on('tiempo',function(tiempo){  
+  if(tiempo<0){
+    //enviar respuesta, usuario
+  }else{
+    $("#tiempo span").text(tiempo);
+  }
+});
+
+//grupo de respuestas
+$("input:checkbox").on('click', function() {
+  // in the handler, 'this' refers to the box clicked on
+  var $box = $(this);
+  if ($box.is(":checked")) {
+    // the name of the box is retrieved using the .attr() method
+    // as it is assumed and expected to be immutable
+    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+    // the checked state of the group/box on the other hand will change
+    // and the current value is retrieved using .prop() method
+    $(group).prop("checked", false);
+    $box.prop("checked", true);
+  } else {
+    $box.prop("checked", false);
+  }
 });
